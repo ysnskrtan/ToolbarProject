827870533c097882a74904b2abe5b45a0efad059
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-11-24 15:12:33 +0300

    Initial Commit

---
+++ ToolbarProject/ToolbarProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarProjectApp.swift
+//  ToolbarProject
+//
+//  Created by Yasin Şükrü Tan on 24.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
